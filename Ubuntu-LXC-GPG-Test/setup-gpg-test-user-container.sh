1f8714dd4d92b84070af03b520a04a0897fed596
GComputeNerd <GComputeNerd@users.noreply.github.com>
2020-05-29 11:15:22 +0530

    More Stuff

--- Ubuntu-LXC-GPG-Test/setup-gpg-test-user-container.sh
+++ Ubuntu-LXC-GPG-Test/setup-gpg-test-user-container.sh
@@ -7,21 +7,30 @@ if [[ $EUID -ne 0  ]]; then
 		exit 99
 fi
 
-echo "Starting Container..."
+echo "Starting Container & Temporary PATH fix..."
 lxc-start $choice
+export PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin
+apt-get install -y curl
+
+curl https://raw.githubusercontent.com/GComputeNerd/gist-collection-private/master/Ubuntu-LXC-GPG-Test/write_to_files_gpg.sh?token=AHFBBH4UPL7SK5BDQRRORJ262CQEQ > write_to_files_gpg.sh
+chmod +x write_to_files_gpg.sh
+
 echo "Updating bashrc...."
-lxc-attach -n $choice -- echo "export PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin" >> ~/.bashrc
+lxc-attach -n $choice -- ./write_to_files_gpg.sh 1
+
 echo "Restarting Container...."
 lxc-stop -n $choice
 lxc-start -n $choice
+
 echo "Installing Requirements"
 lxc-attach -n $choice -- apt-get install gpg vsftpd openssh-server
+
 echo "Setting up gpg"
-lxc-attach -n $choice -- echo "use-agent\npinentry-mode loopback" > ~/.gnupg/gpg.conf
-lxc-attach -n $choice -- echo "allow-loopback pinentry" > ~/.gnupg/gpg-agent.conf
-lxc-attach -n $choice -- echo RELOADAGENT | gpg-connect-agent
+lxc-attach -n $choice -- ./write_to_files_gpg.sh 2
+
 echo "setting up vsftpd"
 lxc-attach -n $choice -- systemctl enable vsftpd
-lxc-attach -n $choice -- echo "write_enable=YES" >> /etc/vsftpd.conf
+lxc-attach -n $choice -- ./write_to_files_gpg.sh 3
 lxc-attach -n $choice -- systemctl restart vsftpd
+
 echo "Done"
