5f2767052d4fa01d8c2df86c751bc6f98bd3f3b7
GComputeNerd <GComputeNerd@users.noreply.github.com>
2020-05-30 10:46:45 +0530

    Fixed a Bug

--- Ubuntu-LXC-GPG-Test/write_to_files_gpg.sh
+++ Ubuntu-LXC-GPG-Test/write_to_files_gpg.sh
@@ -6,7 +6,6 @@ elif [[ $1 = '2' ]]; then
 	echo "use-agent" > ~/.gnupg/gpg.conf
 	echo "pinentry-mode loopback" >> ~/.gnupg/gpg.conf
 	echo "allow-loopback pinentry" > ~/.gnupg/gpg-agent.conf
-	echo RELOADAGENT | gpg-connect-agent
 elif [[ $1 = '3' ]]; then
 	echo "write_enable=YES" >> /etc/vsftpd.conf
 fi
