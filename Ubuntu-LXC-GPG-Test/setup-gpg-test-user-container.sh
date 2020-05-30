83dd0f563c9e14372cb70818fb9159e92e76c475
GComputeNerd <GComputeNerd@users.noreply.github.com>
2020-05-30 09:20:21 +0530

    More fixes

--- Ubuntu-LXC-GPG-Test/setup-gpg-test-user-container.sh
+++ Ubuntu-LXC-GPG-Test/setup-gpg-test-user-container.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-read -p "Container-Name : ", choice
-
 if [[ $EUID -ne 0  ]]; then
 		echo "PLEASE RUN SCRIPT AS ROOT"
 		exit 99
 fi
 
-echo "Starting Container & Temporary PATH fix..."
-lxc-start $choice
+echo "Listing Containers..."
+lxc-ls
+
+read -p "Container-Name : " choice
+lxc-stop -n $choice
+
+set -e
+
+echo "Starting Container & Installing Git"
+lxc-start -n $choice
 lxc-attach -n $choice -- apt-get update
 lxc-attach -n $choice -- apt-get install -y git
 
+echo "Getting write_to_files_gpg.sh"
 lxc-attach -n $choice -- git clone https://github.com/GComputeNerd/random-script-collection.git
-
 lxc-attach -n $choice -- mv random-script-collection/Ubuntu-LXC-GPG-Test/write_to_files_gpg.sh .
 lxc-attach -n $choice -- chmod +x write_to_files_gpg.sh
 
@@ -32,4 +38,9 @@ lxc-attach -n $choice -- systemctl enable vsftpd
 lxc-attach -n $choice -- ./write_to_files_gpg.sh 3
 lxc-attach -n $choice -- systemctl restart vsftpd
 
+lxc-attach -n $choice -- rm -rf random-script-collection
+lxc-attach -n $choice -- rm -rf write_to_files_gpg.sh
 echo "Done"
+
+echo "Displaying Container IP Adress"
+lxc-attach -n $choice -- ip addr show
